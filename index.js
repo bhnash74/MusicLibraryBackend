4ada80a6fcdd3fa578ab32fc14f4a19a0e62e8b3
bhnash74 <bhn774@gmail.com>
2021-10-05 18:48:05 -0500

    added endpoints for getall, get, post, put and delete

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const repoContext = require("./repository/repository-wrapper");
+const app = express();
+app.listen(3000, function () {
+    console.log("Server started.  Listening on port 3000.");
+});
+
+app.get('/api/songs', (req, res) => {
+    const songs = repoContext.songs.findAllSongs();
+    return res.send(songs);
+    });
+app.get('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const song = repoContext.songs.findSongById(id);
+    return res.send(song);
+});
+app.post('/api/songs', (req, res) => {
+    const newSong = req.body;
+    const addedSong = repoContext.songs.createSong(newSong);
+    return res.send(addedSong);
+});
+app.put('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const songPropertiesToUpdate = req.body;
+    const updatedSong = repoContext.songs.updateSong(id, songPropertiesToUpdate);
+    return res.send(updatedSong)
+});
+app.delete('/api/songs/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedDataSet = repoContext.songs.deleteSong(id);
+    return res.send(updatedDataSet);
+});
